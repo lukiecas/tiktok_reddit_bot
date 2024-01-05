2c3b6bc5324e975e2b536cd2ad4ec6553ea3eb4b
lukiecas <84542982+lukiecas@users.noreply.github.com>
2024-01-05 11:55:46 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import praw
+import os
+reddit = praw.Reddit(client_id='OCCThCqol3C9RYbKgOFdeg',
+                     client_secret='5j47nUj0NZq5Br1ikVaTS6GnFupcRA',
+                     user_agent='<console:redditpostcollector:1.0>')
+sub_name = input("enter subreddit name: ")
+i = 0
+for submission in reddit.subreddit(sub_name).top(time_filter="all", limit=1000):
+    text = submission.title + submission.selftext 
+    title = submission.title.replace(" " , "_")
+    text = text.replace("AITA", "Am I in the wrong")
+    with open(f'./amitheasshole/{i}.txt', 'w') as f:
+        f.write(text)
+    i = i+1
+
+
